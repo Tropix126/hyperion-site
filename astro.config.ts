f30064e441c67dc785e53f1ada27eeb6c6454833
Tropical <42101043+Tropix126@users.noreply.github.com>
2022-09-11 00:17:50 -0500

    baseUrl in tsconfig

--- astro.config.ts
+++ astro.config.ts
@@ -6,10 +6,6 @@ import svelte from "@astrojs/svelte";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 
-import { fileURLToPath } from "url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 // https://astro.build/config
 export default defineConfig({
 	integrations: [svelte(), prefetch(), sitemap()],
@@ -17,8 +13,8 @@ export default defineConfig({
 		build: { sourcemap: true },
 		resolve: {
 			alias: {
-				"~": path.resolve(__dirname, "./src")
+				"~": path.resolve("./src")
 			}
 		}
-	}
+	},
 });
