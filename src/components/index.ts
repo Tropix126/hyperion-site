ed0cfc44eabe8236fb51359fb7cd85efde1f347e
Tropical <42101043+Tropix126@users.noreply.github.com>
2022-09-11 00:11:30 -0500

    single entry point for components

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Button } from "./Button.svelte";
+export { default as NavigationHeader } from "./NavigationHeader.svelte";
+export { default as NavigationItem } from "./NavigationItem.svelte";
+export { default as PageSelector } from "./PageSection.svelte";
