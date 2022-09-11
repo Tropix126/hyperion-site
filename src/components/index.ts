ce3d2cc815273d40ae4082b2dce0d5663b837f47
Tropical <42101043+Tropix126@users.noreply.github.com>
2022-09-11 00:38:22 -0500

    i am a moron

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export { default as Button } from "./Button.svelte";
 export { default as NavigationHeader } from "./NavigationHeader.svelte";
 export { default as NavigationItem } from "./NavigationItem.svelte";
-export { default as PageSelector } from "./PageSection.svelte";
+export { default as PageSelector } from "./PageSection.astro";
